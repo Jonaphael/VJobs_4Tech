4c2fd8ee7b9bc21cb8fa2dff7c1c116121922f43
jonaphael <jonaphael4@gmail.com>
2018-07-19 16:11:27 -0300

    Update on put request
    update on put request and added validators

--- server/app.js
+++ server/app.js
@@ -5,28 +5,9 @@ let jobs = require('./config/jobs');
 const Job = require('./models/job');
 
 /* validator */
-//const validator = require('express-validator');
 const { check, validationResult } = require('express-validator/check');
 const { matchedData } = require('express-validator/filter');
-
-const port = 3000;
-const app = express();
-
-const bodyParser = require('body-parser');
-
-/* app */
-app.use(bodyParser.urlencoded({ extended : false }));
-app.use(bodyParser.json());
-//app.use(validator());
-
-/* home */
-app.get('/', async(req, res) => res.send('VJobs work'));
-
-/* get all jobs */
-app.get('/jobs', async (req, res) => res.send(jobs));
-
-/* post a job */
-app.post('/jobs', [
+const validator = [
     /* validations */
     check('id', 'Invalid id must be a number').isNumeric(),
     check('name', 'Invalid name').isLength({ min: 1, max: 255 }).trim(),
@@ -37,8 +18,25 @@ app.post('/jobs', [
     check('responsibilities', 'Invalid name').isLength({ min: 1, max: 255 }).trim(),
     check('isImpaired', 'Invalid name').isBoolean(),
     check('isActive', 'Invalid name').isBoolean()
-    
-], (req ,res) => {
+];
+
+const port = 3000;
+const app = express();
+
+const bodyParser = require('body-parser');
+
+/* app */
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
+
+/* home */
+app.get('/', async(req, res) => res.send('VJobs work'));
+
+/* get all jobs */
+app.get('/jobs', async (req, res) => res.send(jobs));
+
+/* post a job */
+app.post('/jobs', validator , async (req ,res) => {
     
     const errors = validationResult(req);
     
@@ -49,40 +47,26 @@ app.post('/jobs', [
      /* const x = matchedData(req); */
     const job = createJob(matchedData(req));
     jobs.push(job);
-    res.send('Job Added');
+    return res.send('Job Added');
 
 })
 
-            /* other way to post a job */
- /* app.post('/jobs', async (req, res) => {
-    try {
-        // validator 
-        req.check('id', 'Invalid id must be a number').isNumeric();
-        req.check('name', 'Invalid name').isLength({ min: 1, max: 255 });
-        req.check('salary', 'Invalid name').isLength({ min: 1, max: 255 });
-
-
-        const errors = req.validationErrors();
-        if(!errors){
-
-            const jobs_length = jobs.length;
-            let job = createJob(req.body);
-            jobs.push(job);
-    
-            if(jobs.length > jobs_length)
-                return res.send('Job Added');
-        }
-        else return res.status(400).send({ errors : errors});
-
-        return res.status(500).send('Internal Error');
-
-    } catch (error) {
-        return res.status(500).send('Internal Error');
-    }
-}); */
-
 /* update a job by id  *incomplete */
-app.put('jobs/:id',(req, res) => res.send('put') );
+app.put('jobs/:id', validator , async (req, res) => {
+    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped()})
+
+    let index  = await jobs.findIndex( job => job.id == req.params.id);
+
+    if(index > 0){
+        /* change the value */
+    }
+
+    return res.send(400).json({ error : "not found"})
+    res.send('put') 
+});
 
 /* delete a job by id  *incomplete */
 app.delete('/jobs/:id', (req, res) => res.send('delete'));
@@ -116,3 +100,32 @@ const createJob = (obj) => new Job(obj.id,
                                     obj.salary,
                                     obj.isImpaired,
                                     obj.isActive);
+
+
+            /* other way to post a job */
+ /* app.post('/jobs', async (req, res) => {
+    try {
+        // validator 
+        req.check('id', 'Invalid id must be a number').isNumeric();
+        req.check('name', 'Invalid name').isLength({ min: 1, max: 255 });
+        req.check('salary', 'Invalid name').isLength({ min: 1, max: 255 });
+
+
+        const errors = req.validationErrors();
+        if(!errors){
+
+            const jobs_length = jobs.length;
+            let job = createJob(req.body);
+            jobs.push(job);
+    
+            if(jobs.length > jobs_length)
+                return res.send('Job Added');
+        }
+        else return res.status(400).send({ errors : errors});
+
+        return res.status(500).send('Internal Error');
+
+    } catch (error) {
+        return res.status(500).send('Internal Error');
+    }
+}); */
